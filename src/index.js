05857ebb90d4e8fb0dcd8afe3ee15a804cec55a2
chiehyang <anaerobicbacteria@gmail.com>
2018-06-06 08:27:32 +0800

    add comments

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Player from './components/player/';
 
 // need to change to online address
 // otherwise you may have to change address in min.js manually
+// best way to fix this problem is changing webpack config which I have no time
 const MUSIC = {
     title: '天涯歌女',
     artist: '周璇',
