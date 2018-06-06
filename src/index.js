a972a4a74f38adb6a0d67c96dbe3ce1c71ed842d
chiehyang <anaerobicbacteria@gmail.com>
2018-06-06 08:25:28 +0800

    add comment

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 
 import Player from './components/player/';
 
+// need to change to online address
+// otherwise you may have to change address in min.js manually
 const MUSIC = {
     title: '天涯歌女',
     artist: '周璇',
